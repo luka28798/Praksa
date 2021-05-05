c5f616090c7523256bcacfa22943d6b60bdc2822
DESKTOP-D70VD9G\luka2 <luka28798@gmail.com>
2021-05-05 17:30:35 +0200

    3.dan

---
+++ 3.dan/TestProject/TestProject.WebAPI/Controllers/HumanController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Configuration;
+using System.Web;
+//using System.Web.Mvc;
+
+using System.Data.SqlClient;
+namespace TestProject.WebAPI.Controllers
+{
+    public class People
+    {
+        public int HumanID;
+        public string FirstName;
+        public string LastName;
+
+        public People(int HumanID, string FirstName, string LastName)
+        {
+            this.HumanID = HumanID;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+
+        }
+
+        public People() { }
+
+    }
+
+    public class HumanController : ApiController
+    {
+
+
+        public HttpResponseMessage Get(int id)
+        {
+            SqlDataReader reader;
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=tcp:praksamono.database.windows.net,1433;Initial Catalog=Praksa;Persist Security Info=False;User ID=praksa;Password=stipelekic98*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select * FROM People WHERE HumanID=" + id + "";
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            People people = null;
+            while (reader.Read())
+            {
+                people = new People();
+                people.HumanID = Convert.ToInt32(reader.GetValue(0));
+                people.FirstName = reader.GetValue(1).ToString();
+                people.LastName = reader.GetValue(2).ToString();
+
+            }
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, people);
+            return response;
+        }
+
+        [HttpGet]
+        [Route("api/Human/")]
+        public HttpResponseMessage Get()
+        {
+            SqlDataReader reader;
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=tcp:praksamono.database.windows.net,1433;Initial Catalog=Praksa;Persist Security Info=False;User ID=praksa;Password=stipelekic98*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select * FROM People";
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            People human = null;
+            List<People> people = new List<People>();
+            while (reader.Read())
+            {
+                human = new People();
+                human.HumanID = Convert.ToInt32(reader.GetValue(0));
+                human.FirstName = reader.GetValue(1).ToString();
+                human.LastName= reader.GetValue(2).ToString();
+                people.Add(human);
+            }
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, people);
+            return response;
+        }
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] People value)
+        {
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=tcp:praksamono.database.windows.net,1433;Initial Catalog=Praksa;Persist Security Info=False;User ID=praksa;Password=stipelekic98*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "INSERT INTO People (HumanID, FirstName, LastName) VALUES (@HumanID, @FirstName, @LastName);";
+
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+
+            sqlCmd.Parameters.AddWithValue("@HumanID", value.HumanID);
+            sqlCmd.Parameters.AddWithValue("@FirstName", value.FirstName);
+            sqlCmd.Parameters.AddWithValue("@LastName", value.LastName);
+            sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+
+        }
+
+        // DELETE api/values/5
+        public HttpResponseMessage Delete(int id)
+        {
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=tcp:praksamono.database.windows.net,1433;Initial Catalog=Praksa;Persist Security Info=False;User ID=praksa;Password=stipelekic98*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "DELETE FROM People WHERE HumanID =" + id + ";";
+
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody] People value)
+        {
+            if (id != value.HumanID)
+            {
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.BadRequest, "ID se ne smije mijenjati");
+                return response;
+            }
+            else
+            {
+                SqlConnection myConnection = new SqlConnection();
+                myConnection.ConnectionString = @"Server=tcp:praksamono.database.windows.net,1433;Initial Catalog=Praksa;Persist Security Info=False;User ID=praksa;Password=stipelekic98*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+                SqlCommand sqlCmd = new SqlCommand();
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.CommandText = "UPDATE PEOPLE SET HumanID = @HumanID, FirstName = @FirstName, LastName = @LastName WHERE HumanID =" + id + ";";
+
+                sqlCmd.Connection = myConnection;
+                myConnection.Open();
+
+                sqlCmd.Parameters.AddWithValue("@HumanID", value.HumanID);
+                sqlCmd.Parameters.AddWithValue("@FirstName", value.FirstName);
+                sqlCmd.Parameters.AddWithValue("@LastName", value.LastName);
+                sqlCmd.ExecuteNonQuery();
+                myConnection.Close();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+                return response;
+
+
+            }
+        }
+
+    }
+
+}
---
+++ 3.dan/TestProject/TestProject.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Configuration;
+using System.Web;
+//using System.Web.Mvc;
+
+using System.Data.SqlClient;  
+namespace TestProject.WebAPI.Controllers
+{
+    public class Animals
+    {
+        public int AnimalID;
+        public string AnimalType;
+        public string Animal;
+        public int HumanID;
+        public Animals(int AnimalID, string AnimalType, string Animal, int HumanID)
+        {
+            this.AnimalID = AnimalID;
+            this.AnimalType = AnimalType;
+            this.Animal = Animal;
+            this.HumanID = HumanID;
+        }
+
+        public Animals() { }       
+         
+    }
+
+    public class ValuesController : ApiController
+    {
+ 
+
+        public HttpResponseMessage Get(int id)
+        {
+            SqlDataReader reader;
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=tcp:praksamono.database.windows.net,1433;Initial Catalog=Praksa;Persist Security Info=False;User ID=praksa;Password=stipelekic98*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select * FROM Animals WHERE AnimalID=" + id + "";
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            Animals animal = null;
+            while (reader.Read())
+            {
+                animal = new Animals(); 
+                animal.AnimalID = Convert.ToInt32(reader.GetValue(0));
+                animal.AnimalType = reader.GetValue(1).ToString();
+                animal.Animal = reader.GetValue(2).ToString();
+                animal.HumanID = Convert.ToInt32(reader.GetValue(3));
+            }
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, animal);
+            return response;
+        }
+        
+        [HttpGet]
+        [Route("api/Values/")]
+        public HttpResponseMessage Get()
+        {
+            SqlDataReader reader;
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=tcp:praksamono.database.windows.net,1433;Initial Catalog=Praksa;Persist Security Info=False;User ID=praksa;Password=stipelekic98*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select * FROM Animals";
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            Animals animal = null;
+            List<Animals> animals = new List<Animals>();
+            while (reader.Read())
+            {
+                animal = new Animals();
+                animal.AnimalID = Convert.ToInt32(reader.GetValue(0));
+                animal.AnimalType = reader.GetValue(1).ToString();
+                animal.Animal = reader.GetValue(2).ToString();
+                animal.HumanID = Convert.ToInt32(reader.GetValue(3));
+                animals.Add(animal);
+            }
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, animals);
+            return response;
+        }
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] Animals value)
+        {
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=tcp:praksamono.database.windows.net,1433;Initial Catalog=Praksa;Persist Security Info=False;User ID=praksa;Password=stipelekic98*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "INSERT INTO Animals (AnimalID, AnimalType, Animal, HumanID) VALUES (@AnimalID, @AnimalType, @Animal, @HumanID);";
+
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            
+            sqlCmd.Parameters.AddWithValue("@AnimalID",  value.AnimalID);
+            sqlCmd.Parameters.AddWithValue("@AnimalType", value.AnimalType);
+            sqlCmd.Parameters.AddWithValue("@Animal", value.Animal);
+            sqlCmd.Parameters.AddWithValue("@HumanID", value.HumanID);
+            sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+            
+        }
+
+        // DELETE api/values/5
+        public HttpResponseMessage Delete(int id)
+        {  
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=tcp:praksamono.database.windows.net,1433;Initial Catalog=Praksa;Persist Security Info=False;User ID=praksa;Password=stipelekic98*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "DELETE FROM Animals WHERE AnimalID ="+ id + ";";
+
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody] Animals value)
+        {
+            if (id != value.AnimalID)
+            {
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.BadRequest, "ID se ne smije mijenjati");
+                return response;
+            }
+            else
+            {
+                SqlConnection myConnection = new SqlConnection();
+                myConnection.ConnectionString = @"Server=tcp:praksamono.database.windows.net,1433;Initial Catalog=Praksa;Persist Security Info=False;User ID=praksa;Password=stipelekic98*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+                SqlCommand sqlCmd = new SqlCommand();
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.CommandText = "UPDATE ANIMALS SET AnimalID = @AnimalID, AnimalType = @AnimalType, Animal = @Animal, HumanID = @HumanID WHERE AnimalID ="+id+";";
+
+                sqlCmd.Connection = myConnection;
+                myConnection.Open();
+              
+                sqlCmd.Parameters.AddWithValue("@AnimalID", value.AnimalID);
+                sqlCmd.Parameters.AddWithValue("@animalType", value.AnimalType);
+                sqlCmd.Parameters.AddWithValue("@animal", value.Animal);
+                sqlCmd.Parameters.AddWithValue("@HumanID", value.HumanID);
+                sqlCmd.ExecuteNonQuery();
+                myConnection.Close();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+                return response;
+
+               
+            }
+        }
+
+    }
+
+}
